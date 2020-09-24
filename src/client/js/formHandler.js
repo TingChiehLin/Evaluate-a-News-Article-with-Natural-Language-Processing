56233b51806ee0b1e627300c7fe1a6d41fb28f99
Jay Lin <cooloojayoo@gmail.com>
2020-09-24 20:35:15 +1000

    Update 2.0

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,29 +1,6 @@
 import fetch from 'node-fetch';
 import checkForName from './nameChecker';
 
-const updateUI = (data) => {
-    const resultDiv = document.getElementById('results');
-    Object.keys(data).forEach(key =>{
-		const divElement = document.createElement('div');
-		divElement.classList.add(`${key}`);
-		divElement.innerHTML = `<p><strong>${key}: </strong> ${data[key]}</p>`;
-		resultDiv.appendChild(divElement);
-	});
-}
-
-const getUserUrl = () => {
-    const userUrl = document.getElementById('user-url').value;
-    return userUrl;
-}
-
-const handleError = (err) => {
-    if (err.message) {
-        console.log(err.message);
-    } else {
-        console.log("Somthing went wrong!");
-    }
-}
-
 //handle request 
 const sendHttpRequest = (method, url='', data) => {
     return fetch(url,{
@@ -41,18 +18,30 @@ const sendHttpRequest = (method, url='', data) => {
     });
 }
 
+//Update UI
+const updateUI = (data) => {
+    const result = document.getElementById('results');
+    data.array.forEach(element => {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = `<p>${element}</p>`;
+        result.appendChild(newElement);
+    });
+}
+
 //userUrl
 const handleSubmit = (event) => {
     event.preventDefault()
     //reset the results
     document.getElementById('results').innerHTML = '';
+    const userUrl = document.getElementById('user-url').value;
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
+    
     console.log("::: Form Submitted :::")
-    const url = getUserUrl();
-    const data = sendHttpRequest('POST',url);
+    const data = sendHttpRequest('POST',userUrl);
     updateUI(data);
+
     // fetch('http://localhost:8080/test')
     // .then(res => res.json())
     // .then(function(res) {
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,3 @@
- //"test": "echo \"Error: no test specified\" && exit 1",
 var path = require('path')
 const express = require('express')
 const fetch = require('node-fetch')
@@ -22,39 +21,37 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-app.post('/result', async(req,res) =>{
-    try {
-        const nameValues = {
-            'J+': 'Rich',
-            'J': 'lively',
-            'B': 'Friendly',
-            'N': 'Basketball Star',
-            'To': 'Play as actor',
-            'None': 'No Comment'
-        };
-
-        let apiUrl =  `https://api.meaningcloud.com/sentiment-2.1?key=${application_Key}&url=${req.body.userUrl}&lang=en`;
-        let response = await fetch(apiUrl);
-        let data = await response.json();
-
-        if(data.status.msg === 'OK') {
+app.post('/result',(req,res) =>{
+    
+    let apiUrl =  `https://api.meaningcloud.com/sentiment-2.1?key=${application_Key}&url=${req.body.userUrl}&lang=en`;
+    return fetch(apiUrl).then(response => {
+        if (response.json().status.msg === 'OK') {
             const results = {};
             results.credits = data.status.credits;
             results.label = data.category_list.label;
 			results.term = data.category_list.term_list;
-			res.json(evaluation)
-		} else {
-			res.json({msg: data.status.msg})
-		}
-
-    } catch (err) {
-        res.status(500).send({
-            status: 'error',
-        })
-    }
+        } else {
+            return response.json().then(error => {
+                console.log(error);
+                throw new Error('Data Fetch went wrong');
+            })
+        }
+    });
 })
 
-
+return fetch(url,{
+    method: method,
+    body: data,
+}).then(response => {
+    if(response.status >= 200 && response.status < 300) {
+        return response.json();
+    } else {
+        return response.json().then(errData => {
+            console.log(errData);
+            throw new Error('Server went wrong');
+        });
+    }
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
@@ -64,3 +61,12 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+// const nameValues = {
+//     'J+': 'Rich',
+//     'J': 'lively',
+//     'B': 'Friendly',
+//     'N': 'Basketball Star',
+//     'To': 'Play as actor',
+//     'None': 'No Comment'
+// };
